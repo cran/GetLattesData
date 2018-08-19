5a48fcf09f19bf3af2b200204e9efe0f1ae04050
Marcelo Perlin <marceloperlin@gmail.com>
2018-08-19 15:00:11 +0000

    version 1.1

--- inst/doc/gld_vignette-ReadLattes.R
+++ inst/doc/gld_vignette-ReadLattes.R
@@ -1,48 +1,48 @@
 ## ---- echo=FALSE---------------------------------------------------------
-#knitr::opts_chunk$set(eval=FALSE)
+knitr::opts_chunk$set(eval=FALSE)
 
 ## ------------------------------------------------------------------------
-library(GetLattesData)
-
-# ids from EA-UFRGS
-my.ids <- c('K4713546D3','K4723925J2')
-
-# qualis for the field of management
-field.qualis = 'ADMINISTRAÇÃO PÚBLICA E DE EMPRESAS, CIÊNCIAS CONTÁBEIS E TURISMO'
-
-l.out <- gld_get_lattes_data(id.vec = my.ids, field.qualis = field.qualis)
-
-## ------------------------------------------------------------------------
-names(l.out)
+#  library(GetLattesData)
+#  
+#  # ids from EA-UFRGS
+#  my.ids <- c('K4713546D3','K4723925J2')
+#  
+#  # qualis for the field of management
+#  field.qualis = 'ADMINISTRAÇÃO PÚBLICA E DE EMPRESAS, CIÊNCIAS CONTÁBEIS E TURISMO'
+#  
+#  l.out <- gld_get_lattes_data(id.vec = my.ids, field.qualis = field.qualis)
 
 ## ------------------------------------------------------------------------
-tpesq <- l.out$tpesq
-str(tpesq)
+#  names(l.out)
 
 ## ------------------------------------------------------------------------
-dplyr::glimpse(l.out$tpublic.published)
+#  tpesq <- l.out$tpesq
+#  str(tpesq)
 
 ## ------------------------------------------------------------------------
-tpublic.published <- l.out$tpublic.published
-
-library(ggplot2)
-
-p <- ggplot(tpublic.published, aes(x = qualis)) +
-  geom_bar(position = 'identity') + facet_wrap(~name) +
-  labs(x = paste0('Qualis: ', field.qualis))
-print(p)
+#  dplyr::glimpse(l.out$tpublic.published)
 
 ## ------------------------------------------------------------------------
-library(dplyr)
+#  tpublic.published <- l.out$tpublic.published
+#  
+#  library(ggplot2)
+#  
+#  p <- ggplot(tpublic.published, aes(x = qualis)) +
+#    geom_bar(position = 'identity') + facet_wrap(~name) +
+#    labs(x = paste0('Qualis: ', field.qualis))
+#  print(p)
 
-my.tab <- tpublic.published %>%
-  group_by(name) %>%
-  summarise(n.papers = n(),
-            max.SJR = max(SJR, na.rm = T),
-            mean.SJR = mean(SJR, na.rm = T),
-            n.A1.qualis = sum(qualis == 'A1', na.rm = T),
-            n.A2.qualis = sum(qualis == 'A2', na.rm = T),
-            median.authorship = median(as.numeric(order.aut), na.rm = T ))
-
-knitr::kable(my.tab)
+## ------------------------------------------------------------------------
+#  library(dplyr)
+#  
+#  my.tab <- tpublic.published %>%
+#    group_by(name) %>%
+#    summarise(n.papers = n(),
+#              max.SJR = max(SJR, na.rm = T),
+#              mean.SJR = mean(SJR, na.rm = T),
+#              n.A1.qualis = sum(qualis == 'A1', na.rm = T),
+#              n.A2.qualis = sum(qualis == 'A2', na.rm = T),
+#              median.authorship = median(as.numeric(order.aut), na.rm = T ))
+#  
+#  knitr::kable(my.tab)
 
